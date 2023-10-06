87e0c69a79fc763cb54a6f528c4c52618bb3574a
aziza aminova <aziza@acompleteappliance.com>
2023-10-06 15:19:16 -0400

    from 2 file

--- calculator.js
+++ calculator.js
@@ -1,2 +1,15 @@
-2+2
-3+3
+function appendToDisplay(value) {
+    document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+function calculateResult() {
+    try {
+        document.getElementById("display").value = eval(document.getElementById("display").value);
+    } catch (error) {
+        document.getElementById("display").value = "Error";
+    }
+}
