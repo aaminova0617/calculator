6c05aa1b2051cd79bd6d3c3712c3cb2336460c13
aziza aminova <aziza@acompleteappliance.com>
2023-10-08 13:51:55 -0400

    input from keyb

--- calculator.js
+++ calculator.js
@@ -13,3 +13,22 @@ function calculateResult() {
         document.getElementById("display").value = "Error";
     }
 }
+
+// Listen for keyboard input
+document.addEventListener("keydown", function(event) {
+    const key = event.key;
+
+    if (/[0-9+\-*/=]/.test(key)) {
+        event.preventDefault(); // Prevent the default behavior of keys
+        if (key === "=") {
+            calculateResult();
+        } else {
+            appendToDisplay(key);
+        }
+    } else if (key === "Enter") {
+        event.preventDefault(); // Prevent Enter key from submitting forms
+        calculateResult();
+    } else if (key === "Escape") {
+        clearDisplay();
+    }
+});
